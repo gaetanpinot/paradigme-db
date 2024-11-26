7bc4b211dd91fe8bab5bb3052638c4081c0f7293
Yehor Prykhodko <yehor.prykhodko8@etu.univ-lorraine.fr>
2024-11-26 15:04:34 +0200

    article

--- td1/src/index.php
+++ td1/src/index.php
@@ -1,4 +1,14 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+
+use iutnc\hellokant\model\Article;
 use iutnc\hellokant\query\Query;
-Query::table('employeuh')->where('nom','=','Pierre')->select(['nom', 'prenom', 'age'])->get();
+//Query::table('employeuh')->where('nom','=','Pierre')->select(['nom', 'prenom', 'age'])->get();
+
+
+//Article test
+$article  = new Article(['title' => 'Hello', 'content' => 'World']);
+echo $article->title;
+echo $article->content;
+$article->title = 'Bonjour';
+var_dump($article);
